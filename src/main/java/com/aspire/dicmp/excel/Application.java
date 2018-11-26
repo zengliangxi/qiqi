45fac4ac764e7e53f7622aac28724a89b09ddc62
zw_zengliangxi <zengliangxi@aspirecn.com>
2018-11-26 17:55:42 +0800

    项目初始化

--- src/main/java/com/aspire/dicmp/excel/Application.java
+++ src/main/java/com/aspire/dicmp/excel/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 项目名称:新零售-合作伙伴管理
  * 包名称:  com.aspire.dicmp.excel
  * 类名称:  Application
- * 类描述:  ${功能描述}
+ * 类描述:  excel工具测试
  * 创建人:  zlx
  * 创建时间:2018年10月18日 10:31
  */
